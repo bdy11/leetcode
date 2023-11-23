da1de91f2d55231b15adea39f3409d55be964b77
dongyue.bai <dongyue.bai@zongmutech.com>
2023-11-23 15:53:48 +0100

    combination sum

--- src/09_backtracking/39_combination-sum.cpp
+++ src/09_backtracking/39_combination-sum.cpp
@@ -1,3 +1,9 @@
+/*
+递归三部曲
+1.确定函数参数及返回值
+2. 确定递归终止条件
+3. 确定单层递归逻辑
+*/
 class Solution {
 private:
     vector<int> path;
@@ -11,15 +17,9 @@ private:
             return;
         }
 
-        for(int i = index; i < candidates.size();i++){
+        for(int i = index; i < candidates.size() && sum + candidates[i] <= target;i++){
             sum += candidates[i];
             path.push_back(candidates[i]);
-
-            // print out path to show the back tracking steps
-            for(int j = 0; j < path.size(); j++){
-                cout<< path[j]<< " ";
-            }
-            cout<<endl;
             back_tracking(candidates, target, sum, i);
             sum-= candidates[i];
             path.pop_back();
@@ -27,6 +27,7 @@ private:
 }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         back_tracking(candidates, target, 0, 0);
         return result;
     }
